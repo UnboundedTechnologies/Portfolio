c2f1f9e766e724862f5c65b277b08f75ce1c7230
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2022-07-15 02:36:01 +0200

    unused import Divider deleted

--- pages/projects.js
+++ pages/projects.js
@@ -1,4 +1,4 @@
-import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
 import { WorkGridItem } from '../components/grid-item'
