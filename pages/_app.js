54e367cc01f6b828f3f8c612e307a3cc5f8c6e41
Unbounded Technologies <unboundedtechnologies@gmaiL.com>
2023-02-18 23:20:35 +0100

    Added @vercel/analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import Layout from '../components/layouts/main'
 import Fonts from '../components/fonts'
 import { AnimatePresence } from 'framer-motion'
 import Chakra from '../components/chakra'
+import { Analytics } from '@vercel/analytics/react';
 
 if (typeof window !== 'undefined') {
   window.history.scrollRestoration = 'manual'
@@ -22,6 +23,7 @@ function Website({ Component, pageProps, router }) {
           }}
         >
           <Component {...pageProps} key={router.route} />
+          <Analytics />
         </AnimatePresence>
       </Layout>
     </Chakra>
